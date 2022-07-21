237a976c50cbe194b0cf4c1eaeea88c4a8b5c062
yastrebow <yastrebow@gmail.com>
2022-07-21 09:14:08 +0300

    Final version of MS 'analyzer'

--- src/main/java/ru/yastrebov/analyzer/kafka/KafkaConsumer.java
+++ src/main/java/ru/yastrebov/analyzer/kafka/KafkaConsumer.java
@@ -6,7 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import ru.yastrebov.analyzer.service.impl.AnalyzerServiceImpl;
-import ru.yastrebov.requestAnalyzerLib.model.RequestDTO;
+import ru.yastrebov.requestAnalyzerLib.model.Request;
 
 
 @Slf4j
@@ -16,9 +16,9 @@ public class KafkaConsumer {
 
     private final AnalyzerServiceImpl analyzerService;
 
-    @KafkaListener(topics = "loan_requests", groupId = "${spring.kafka.consumer.group-id}")
+    @KafkaListener(topics = "requests_for_loan", groupId = "${spring.kafka.consumer.group-id}")
 
-    private void listenGroupRequests(ConsumerRecord<String, RequestDTO> message) {
+    private void listenGroupRequests(ConsumerRecord<String, Request> message) {
 
         log.info("Received message: {}", message);
 
